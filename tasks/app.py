0d9d9b55ca52a8fe93388937403539f5322f3845
Santiago <s.patino@uniandes.edu.co>
2024-10-23 13:56:53 -0500

    update db conection tasks microservice

--- tasks/app.py
+++ tasks/app.py
@@ -11,8 +11,8 @@ from os import environ
 import os
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/flask_database'
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@34.134.212.111:5432/flask_database'
+#app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 UPLOAD_FOLDER = os.path.join(app.root_path, '../videos')
