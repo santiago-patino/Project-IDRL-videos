f081f83869b6c6a9d4884eb4bb54eae0655560f8
Santiago <s.patino@uniandes.edu.co>
2024-10-24 17:28:50 -0500

    app process_video env problem

--- process_videos/app.py
+++ process_videos/app.py
@@ -1,16 +1,10 @@
 from flask import Flask
 from modelos import db
-import os
 from os import environ
 
-import json
-
-# with open('../config.json') as config_file:
-#     config = json.load(config_file)
 
 app = Flask(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/flask_database'
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
