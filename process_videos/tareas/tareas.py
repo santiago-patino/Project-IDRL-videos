66cdcae5d8014962e62660727ced5a2bc72fc625
Santiago <s.patino@uniandes.edu.co>
2024-11-06 19:49:02 -0500

    app context tareas

--- process_videos/tareas/tareas.py
+++ process_videos/tareas/tareas.py
@@ -3,7 +3,7 @@ import os
 ruta_modelos = os.path.join(os.path.dirname(__file__), '../../modelos')
 sys.path.append(ruta_modelos)
 
-from celery import Celery
+#from celery import Celery
 from modelos import db, Task
 import app
 import requests
@@ -27,57 +27,59 @@ def editar_video(message):
     
     print(f'task id: {task_id} queue recibida!!!!!')
     
-    task = Task.query.get(task_id)
-    
-    if task:
-        filename = task.nombre_video
-        original_file_path = os.path.join(str(task_id), filename)
-        #original_file_path = os.path.join(f'{str(task_id)}', filename)
-        #print(original_file_path)
-        
-        temp_path = os.path.join(f'/tmp/{str(task_id)}')
-        os.makedirs(temp_path, exist_ok=True)
-        path_video_download = f'{temp_path}/{filename}'
-        
-        download_video(original_file_path, path_video_download)
-        directory = os.path.dirname(path_video_download)
-        
-        if os.path.exists(directory):
-            image_path = "../images/logo.png"
-            video_clip = VideoFileClip(path_video_download)
+    with current_app.app_context():
+        task = Task.query.get(task_id)
+        if task:
+            filename = task.nombre_video
+            original_file_path = os.path.join(str(task_id), filename)
+            #original_file_path = os.path.join(f'{str(task_id)}', filename)
+            #print(original_file_path)
             
-            video_aspect_ratio = 16 / 9
-            video_width, video_height = video_clip.size
-            new_width = video_width
-            new_height = int(new_width / video_aspect_ratio)
+            temp_path = os.path.join(f'/tmp/{str(task_id)}')
+            os.makedirs(temp_path, exist_ok=True)
+            path_video_download = f'{temp_path}/{filename}'
             
-            if new_height > video_height:
-                new_height = video_height
-                new_width = int(new_height * video_aspect_ratio)
+            download_video(original_file_path, path_video_download)
+            directory = os.path.dirname(path_video_download)
+            
+            if os.path.exists(directory):
+                image_path = "../images/logo.png"
+                video_clip = VideoFileClip(path_video_download)
                 
-            cropped_video = video_clip.crop(width=new_width, height=new_height, x_center=video_width/2, y_center=video_height/2)
-            
-            max_duration = 20
-            if cropped_video.duration > max_duration:
-                cropped_video = cropped_video.subclip(0, max_duration)
-            
-            image_logo = ImageClip(image_path).set_duration(3).resize(cropped_video.size)
-            final_video = concatenate_videoclips([image_logo, cropped_video, image_logo])
+                video_aspect_ratio = 16 / 9
+                video_width, video_height = video_clip.size
+                new_width = video_width
+                new_height = int(new_width / video_aspect_ratio)
                 
-            new_file_name = f"edited_{filename}"
-            edited_file_path = os.path.join(temp_path, new_file_name)
-            
-            final_video.write_videofile(edited_file_path, fps=cropped_video.fps, codec='libx264')
-            upload_video(edited_file_path, f'{str(task.id)}/{new_file_name}');
-            
-            task.status = "processed"
-            
-            db.session.commit()
+                if new_height > video_height:
+                    new_height = video_height
+                    new_width = int(new_height * video_aspect_ratio)
+                    
+                cropped_video = video_clip.crop(width=new_width, height=new_height, x_center=video_width/2, y_center=video_height/2)
+                
+                max_duration = 20
+                if cropped_video.duration > max_duration:
+                    cropped_video = cropped_video.subclip(0, max_duration)
+                
+                image_logo = ImageClip(image_path).set_duration(3).resize(cropped_video.size)
+                final_video = concatenate_videoclips([image_logo, cropped_video, image_logo])
+                    
+                new_file_name = f"edited_{filename}"
+                edited_file_path = os.path.join(temp_path, new_file_name)
+                
+                final_video.write_videofile(edited_file_path, fps=cropped_video.fps, codec='libx264')
+                upload_video(edited_file_path, f'{str(task.id)}/{new_file_name}');
+                
+                task.status = "processed"
+                
+                db.session.commit()
+            else:
+                print(f"Directorio no existe: {task_id}")
         else:
-            print(f"Directorio no existe: {task_id}")
-    else:
-        print(f"Tarea con id {task_id} no encontrada")
-    message.ack()
+            print(f"Tarea con id {task_id} no encontrada")
+        message.ack()
+    
+    
             
 def download_video(source_blob_name, destination_file_path):
     client = storage.Client()
