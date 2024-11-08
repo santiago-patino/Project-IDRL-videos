430fc30791dbfd0940df2fd0df49d401f1b5a459
Santiago <s.patino@uniandes.edu.co>
2024-11-08 17:20:46 -0500

    revert position confirm message

--- process_videos/tareas/tareas.py
+++ process_videos/tareas/tareas.py
@@ -22,9 +22,9 @@ subscription_path = f'projects/{project_id}/subscriptions/{sub_id}'
 def callback(message, app_context):
     # Usa el contexto de la aplicación Flask explícitamente
     with app_context:
-        message.ack() #confirma antes de editarlo    
         print("Mensaje recibido:", message.data.decode('utf-8'))
         editar_video(int(message.data.decode('utf-8')))
+        message.ack() #confirma la tarea   
 
 # @celery_app.task(name="process.video")
 def editar_video(task_id):
