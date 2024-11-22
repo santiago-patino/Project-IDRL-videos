713022c511d0cc2d77d94eb1a5e3a5dd625e691a
Santiago <s.patino@uniandes.edu.co>
2024-11-22 14:04:05 -0500

    changes push sub

--- process_videos/app.py
+++ process_videos/app.py
@@ -3,42 +3,37 @@ import os
 ruta_modelos = os.path.join(os.path.dirname(__file__), '../modelos')
 sys.path.append(ruta_modelos)
 
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_restful import Api
 from modelos import db
 from os import environ
 from dotenv import load_dotenv
 import threading
 
-from tareas import iniciar_suscripcion
+#from tareas import editar_video
 
 load_dotenv()
 
 app = Flask(__name__)
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:1234@flask_db:5432/flask_database"
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app_context = app.app_context()
 app_context.push()
 db.init_app(app)
-        
-thread = threading.Thread(target=iniciar_suscripcion, args=(app_context,))
-thread.start()
+# thread = threading.Thread(target=iniciar_suscripcion, args=(app_context,))
+# thread.start()
 
-# def iniciar_suscriptor():
-#     app_context = app.app_context()
-#     app_context.push()
-#     db.init_app(app)
-    
-#     iniciar_suscripcion(app_context)
-    
-    #thread = threading.Thread(target=iniciar_suscripcion, args=(app_context,))
-    #thread.start()
+@app.route('/process-message', methods=['POST'])
+def process_message():
+    message = request.get_json()
+    print("Mensaje recibido:", message)
+    print(message.data.decode('utf-8'))
+    #editar_video(int(message))
+    return jsonify({'status': 'Message processed'}), 200
 
-#db.init_app(app)
 
-#iniciar_suscriptor()
 
 
 
